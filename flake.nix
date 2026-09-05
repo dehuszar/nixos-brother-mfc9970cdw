f08f689b7bbc3964c89ce9103b9a0cd4f59ac5eb
Samuel Allen <dehuszar@gmail.com>
2026-09-05 07:58:10 -0400

    write combined dll.conf and set
     SANE_CONFIG_DIR

--- flake.nix
+++ flake.nix
@@ -119,19 +119,26 @@
               ];
             };
 
-            # SANE reads dll.d from the sane-backends package, not from /etc.
-            # We need to create /etc/sane.d/dll.d/brother4 so scanimage loads
-            # the brother4 backend library.
+            # SANE reads dll.d from the sane-backends store path. The
+            # hardware.sane module creates a sane-config package that includes
+            # all backends, but scanimage from simple-scan's sane-backends dep
+            # doesn't use it. We write a combined dll.conf to /etc/sane.d
+            # and tell SANE to look there via SANE_CONFIG_DIR.
             environment.etc = lib.mkIf cfg.enableScanner {
-              "sane.d/dll.d/brother4" = lib.mkIf (cfg.scannerBackend == "brscan4") {
-                text = "brother4\n";
+              "sane.d/dll.conf" = {
+                text =
+                  let
+                    saneDllConf = builtins.readFile "${pkgs.sane-backends}/etc/sane.d/dll.conf";
+                    backend = if cfg.scannerBackend == "brscan4" then "brother4"
+                      else if cfg.scannerBackend == "brscan5" then "brother5"
+                      else "airscan";
+                  in
+                    saneDllConf + "\n" + backend + "\n";
               };
-              "sane.d/dll.d/brother5" = lib.mkIf (cfg.scannerBackend == "brscan5") {
-                text = "brother5\n";
-              };
-              "sane.d/dll.d/airscan" = lib.mkIf (cfg.scannerBackend == "airscan") {
-                text = "airscan\n";
-              };
+            };
+
+            environment.sessionVariables = lib.mkIf cfg.enableScanner {
+              SANE_CONFIG_DIR = "/etc/sane.d";
             };
 
             # Brother's proprietary SANE backends need support files at
